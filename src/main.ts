ba1170c150f1f8449b5c9c4866deeb2558034c29
EduM22 <38257387+EduM22@users.noreply.github.com>
2022-02-23 20:21:20 +0100

    setup vite + tailwindcss

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+<h1 class="text-3xl font-bold underline text-center">
+  Hello world!
+</h1>
+`
