77958a737168aa65d47641b946a323692b1ef01f
EduM22 <38257387+EduM22@users.noreply.github.com>
2022-09-13 16:26:57 +0200

    website basic style

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,30 @@
 export default function Home() {
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
-      <p class="my-6">
-        Welcome to `turquoze`
-      </p>
+    <div class="bg-gray-100 dark:bg-black h-screen min-h-full">
+      <div class="p-4 mx-auto max-w-screen-md">
+        <div class="ml-2 flex lg:ml-0">
+          <a href="/">
+            <div
+              class="h-8 w-8 rounded-md"
+              style={{ backgroundColor: "#40E0D0" }}
+            >
+            </div>
+          </a>
+          <h1 class="ml-2 mt-1 dark:text-white">Turquoze</h1>
+        </div>
+        <div class="pt-24 pb-12">
+          <p class="my-6 dark:text-white">
+            Welcome to Turquoze, a headless e-commerce project. (Currently in
+            private development)
+          </p>
+          <a
+            class="dark:text-white hover:text-blue-600"
+            href="https://github.com/turquoze"
+          >
+            Check our GitHub org for updates
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
